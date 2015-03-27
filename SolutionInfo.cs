21ce1bcf88c29fe8b37b72b7d8d57399808ea8e6
Shannon <sdeminick@gmail.com>
2015-03-27 17:33:43 +1100

    Updates assembly infos

---
+++ SolutionInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Shannon Deminick")]
+[assembly: AssemblyProduct("Umbraco LinqPad Driver")]
+[assembly: AssemblyCopyright("Copyright © Shannon Deminick 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- UmbracoLinqPad/Properties/AssemblyInfo.cs
+++ UmbracoLinqPad/Properties/AssemblyInfo.cs
@@ -6,13 +6,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UmbracoLinqPad")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Umbraco LinqPad Driver")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UmbracoLinqPad")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3b026495-1b72-4110-9ccb-abf352d7798d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
